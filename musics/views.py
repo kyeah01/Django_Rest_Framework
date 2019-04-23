60cf6ad25fe6f60dcbf584671992539b4ec6afe4
kyeah01 <kyeah01@gmail.com>
2019-04-23 06:40:07 +0000

    05 | music detail serializer

--- musics/views.py
+++ musics/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Music, Artist, Comment
-from .serializers import MusicSerializer, ArtistSerializer, ArtistDetailSerializer, CommentSerializer
+from .serializers import MusicSerializer, ArtistSerializer, ArtistDetailSerializer, CommentSerializer, MusicDetailSerializer
 
 # Create your views here.
 
@@ -23,7 +23,7 @@ def music_list(request):
 @api_view(['GET'])
 def music_detail(request, music_pk):
     musics = get_object_or_404(Music, pk=music_pk)
-    serializer = MusicSerializer(musics)
+    serializer = MusicDetailSerializer(musics)
     return Response(serializer.data)
     
 @api_view(['GET'])
